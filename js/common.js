b9a30b5d8b75fe4c90c734a51eca50c09fe9fae8
Ivan Sanz Carasa <ivansanzcarasa@gmail.com>
2021-11-13 17:17:13 +0100

    remove ionicons

--- js/common.js
+++ js/common.js
@@ -72,19 +72,19 @@ $(document).ready(function () {
   // Instagram Feed
   ======================= */
   // userId and accessToken from Matthew Elsom (https://codepen.io/matthewelsom/pen/zrrrLN) for example, for which he thanks a lot!
-  var instagramFeed = new Instafeed({
-    get: "user",
-    limit: 6,
-    resolution: "standard_resolution",
-    userId: "8987997106",
-    accessToken: "8987997106.924f677.8555ecbd52584f41b9b22ec1a16dafb9",
-    template:
-      '<li class="instagram-item"><a href="{{link}}" aria-label="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}"></a></li>',
-  });
+  // var instagramFeed = new Instafeed({
+  //   get: "user",
+  //   limit: 6,
+  //   resolution: "standard_resolution",
+  //   userId: "8987997106",
+  //   accessToken: "8987997106.924f677.8555ecbd52584f41b9b22ec1a16dafb9",
+  //   template:
+  //     '<li class="instagram-item"><a href="{{link}}" aria-label="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}"></a></li>',
+  // });
 
-  if ($("#instafeed").length) {
-    instagramFeed.run();
-  }
+  // if ($("#instafeed").length) {
+  //   instagramFeed.run();
+  // }
 
   /* =======================
   // Scroll Top Button
