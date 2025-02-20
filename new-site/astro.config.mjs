29f7fe3a8e35ad2f30f54ccecb2e196e39385505
Ivan Sanz Carasa <me@ivansanz.com>
2025-02-20 19:34:08 +0100

    arreglando cosas

--- new-site/astro.config.mjs
+++ new-site/astro.config.mjs
@@ -19,6 +19,9 @@ export default defineConfig({
   markdown: {
     rehypePlugins: [[externalLink, {}]],
   },
+  // image: {
+  //   layout: "responsive",
+  // },
   redirects: {
     awd: "test",
   },
