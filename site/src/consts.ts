6af3cac2c8c9b3d8342b0cf9a03cbd5992990e3b
Ivan Sanz Carasa <ivansanzcarasa@gmail.com>
2025-04-20 18:52:30 +0200

    fix description

--- site/src/consts.ts
+++ site/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Centro Núria León"
-export const SITE_DESCRIPTION = "Welcome to my website!"
+export const SITE_TITLE = "Centro Núria León - Psicología y Terapia en Barcelona"
+export const SITE_DESCRIPTION = "Centro Núria León, Psicología y Terapia - Sesiones de Psicología Presenciales en Barcelona y Online"
